cb7169b9267114b7e2ea3a71b48b8be911f0e010
Stephannie Mtz <56009667+FanyMtz@users.noreply.github.com>
2022-05-25 10:16:41 -0500

    Delete codigo.js

--- Exam/codigo.js
+++
@@ -1,9 +0,0 @@
-const cargarOrdenes = async() => {
-
-    let options = {
-        url: 'https://eshop-deve.herokuapp.com/api/v2/orders4',
-        method: 'GET',
-        json: true,
-        headers: {'Authorization': eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ}
-    }
-}
